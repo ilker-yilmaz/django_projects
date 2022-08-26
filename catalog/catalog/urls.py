cdf779b1d0b7f5659215f5e54defccfd7e69d686
ilker yilmaz <ilkery549@gmail.com>
2022-08-26 14:40:28 +0300

    added pages app

--- catalog/catalog/urls.py
+++ catalog/catalog/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# http://127.0.0.1:8000
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
