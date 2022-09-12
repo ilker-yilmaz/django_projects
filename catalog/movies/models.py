0e45405c3062629d671862ffb82550d8d91a77d8
ilker yilmaz <ilkery549@gmail.com>
2022-09-12 23:37:05 +0300

    admin panel changes

--- catalog/movies/models.py
+++ catalog/movies/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.CharField(max_length=50)
-    created_date=models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, verbose_name='Film Adı')
+    description = models.TextField(verbose_name='Film Açıklaması')
+    image = models.CharField(max_length=50, verbose_name='Film Resmi')
+    created_date=models.DateTimeField(auto_now_add=True, verbose_name='Oluşturulma Tarihi')
+    isPublished = models.BooleanField(default=True, verbose_name='Yayında mı?')
+
+
+    def __str__(self):
+        return self.name
     
